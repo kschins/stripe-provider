9b1a236c3ad592d9c59af50f84f75360ca5067f6
Kasey <me@kaseyschindler.com>
2020-02-22 15:35:12 -0700

    Remove unused account number field.

--- Sources/Stripe/Models/Connect/ExternalBankAccount.swift
+++ Sources/Stripe/Models/Connect/ExternalBankAccount.swift
@@ -9,7 +9,6 @@
 // Not expected to be returned by the API at all.
 public struct StripeExternalBankAccount: ExternalAccount, StripeModel {
     public var object: String = "bank_account"
-    public var accountNumber: String?
     public var country: String?
     public var currency: StripeCurrency?
     public var accountHolderName: String?
@@ -19,7 +18,6 @@ public struct StripeExternalBankAccount: ExternalAccount, StripeModel {
     
     public enum CodingKeys: String, CodingKey {
         case object
-        case accountNumber = "account_number"
         case country
         case currency
         case accountHolderName = "account_holder_name"
